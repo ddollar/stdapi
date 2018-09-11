f84a925c3ea2659987753f6b96ae19f90b68164c
David Dollar <david@dollar.io>
2018-09-10 23:29:53 -0400

    per-request templates

--- template.go
+++ template.go
@@ -12,52 +12,33 @@ import (
 )
 
 var (
-	helpers   TemplateHelpers
-	templates = map[string]*template.Template{}
+	templateDir     string
+	templateHelpers TemplateHelpers
 )
 
 type TemplateHelpers func(c *Context) template.FuncMap
 
-func LoadTemplates(dir string, fn TemplateHelpers) error {
-	helpers = fn
-
-	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !info.IsDir() {
-			files := []string{}
-
-			files = appendIfExists(files, filepath.Join(dir, "layout.tmpl"))
-			files = appendIfExists(files, filepath.Join(filepath.Dir(path), "layout.tmpl"))
-			files = append(files, path)
-
-			t, err := template.New("main").ParseFiles(files...)
-			if err != nil {
-				return err
-			}
-
-			rel, err := filepath.Rel(dir, path)
-			if err != nil {
-				return err
-			}
-
-			templates[rel] = t
-		}
-
-		return nil
-	})
+func LoadTemplates(dir string, helpers TemplateHelpers) {
+	templateDir = dir
+	templateHelpers = helpers
 }
 
 func RenderTemplate(c *Context, path string, params interface{}) error {
-	t, ok := templates[fmt.Sprintf("%s.tmpl", path)]
-	if !ok {
-		return fmt.Errorf("no such template: %s", path)
+	files := []string{}
+
+	files = appendIfExists(files, filepath.Join(templateDir, "layout.tmpl"))
+	files = appendIfExists(files, filepath.Join(templateDir, filepath.Dir(path), "layout.tmpl"))
+	files = append(files, filepath.Join(templateDir, fmt.Sprintf("%s.tmpl", path)))
+
+	ts := template.New("main")
+
+	if templateHelpers != nil {
+		ts = ts.Funcs(templateHelpers(c))
 	}
 
-	if helpers != nil {
-		t = t.Funcs(helpers(c))
+	t, err := ts.ParseFiles(files...)
+	if err != nil {
+		return err
 	}
 
 	var buf bytes.Buffer
